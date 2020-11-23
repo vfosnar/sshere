ea99ce388362a6304481361ccd0eee250b2b1551
Vojtěch F <v.fosnar@seznam.cz>
2020-11-23 18:06:01 +0100

    Added a lot of comments and created config.json file

--- main.py
+++ main.py
@@ -1,41 +1,72 @@
-servers = {
-    "name@example.com":{"port":2222},
-    "name2@example2.com":{"passwd":"examplepass", "win":True}
-}
+import os, json
 
-import os
+# Load config file
+servers = json.loads(open(os.path.join(os.path.dirname(__file__), 'config.json'), 'rb').read().decode())
 
-mnt = os.popen("df --output=source .").read().split('\n')[1]
-tar = os.popen("df --output=target .").read().split('\n')[1]
+if __name__ == "__main__":
+    # name@address:/path/on/server
+    mnt = os.popen("df --output=source .").read().split('\n')[1]
+    # /local/mount/path
+    tar = os.popen("df --output=target .").read().split('\n')[1]
 
-addr = mnt.split(":")[0]
-base = ":".join(mnt.split(":")[1:])
-
-server = os.path.join(base, os.getcwd()[len(tar):][1:])
-
-port = 22
-passwd = None
-win = False
-
-if(addr in servers):
-    if("port" in servers[addr]):
-        port = servers[addr]['port']
-    if("passwd" in servers[addr]):
-        passwd = servers[addr]['passwd']
-    if("win" in servers[addr]):
-        win = servers[addr]['win']
+    if(mnt[:len('/dev')] == '/dev'):
+        print("Only sftp is supported..")
+        exit(1)
+    # name@address
+    addr = mnt.split(":")[0]
     
+    # /path/on/server
+    base = ":".join(mnt.split(":")[1:])
 
-ssh = 'ssh -t {} -p {} "cd \\"{}\\"; bash --login"'
+    # Get current working directory and strip out mount point
+    # [1:] -> get rid if / on the beggining and thus make it relative path
+    rels = os.getcwd()[len(tar):][1:]
 
-if(win): # Is computer running on windows
-    ssh = 'ssh -t {} -p {} cmd /K "cd "{}""'
-    server = server[1:]
-    if(server[-1] != '/'):
-        server += '/'
+    # Change it to absolute path by adding server path to the mount point
+    server = os.path.join(base, rels)
 
-if(passwd): # sudo apt install sshpass
-    os.system('sshpass -p {} '.format(passwd) + ssh.format(addr, port, server))
-    pass
-else:
-    os.system(ssh.format(addr, port, server))
+    # Defaults
+    port = 22
+    passwd = None
+    win = False
+
+    servers_name = None
+    
+    # Check if name@address is in the config
+    if(addr in servers):
+        servers_name = addr
+    
+    # Check if name@address:/local/path is in the config
+    elif("{}:{}".format(addr, tar) in servers):
+        servers_name = "{}:{}".format(addr, tar)
+
+    # Load ssh configuration from config.json
+    if(servers_name):
+        if("port" in servers[servers_name]):
+            port = servers[servers_name]['port']
+        if("passwd" in servers[servers_name]):
+            passwd = servers[servers_name]['passwd']
+        if("win" in servers[servers_name]):
+            win = servers[servers_name]['win']
+        
+    # command used to connect to the linux server
+    ssh = 'ssh -t {} -p {} "cd \\"{}\\"; bash --login"'
+
+    if(win): # Is computer running on windows
+        # windows has different syntax
+        ssh = 'ssh -t {} -p {} cmd /K "cd "{}""'
+        # Win filesystem begins with C: not /C:
+        server = server[1:]
+        # Windows wont cd into C: must be C:/
+        if(server[-1] != '/'):
+            server += '/'
+
+    print("Connecting to {} at port {}".format(addr, port))
+    # if passwd is specified in config.json file then sshpass is used
+    # sshpass must be already installed:
+    #  sudo apt install sshpass
+    if(passwd):
+        os.system('sshpass -p {} '.format(passwd) + ssh.format(addr, port, server))
+        pass
+    else:
+        os.system(ssh.format(addr, port, server))
